cb187c450fc30f077322e38376bce00a0fdce616
Ajay Ambule <ajayambule2003@gmail.com>
2025-12-10 22:58:08 +0530

    Student App with spring

--- StudentApp1/src/main/java/com/controller/StudentContoller.java
+++ StudentApp1/src/main/java/com/controller/StudentContoller.java
@@ -2,28 +2,13 @@ package com.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-// @GetMapping =@RequestMapping(value = "/new", method = RequestMethod.GET)
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.model.Student;
-import com.service.StudentService; 
-
 @RequestMapping("/students")
 @Controller
 public class StudentContoller {
 
 	private StudentService service;
 
-	// http://localhost:8080/StudentApp/students/
+	//  http://localhost:8080/StudentApp/students/
 	@GetMapping("/") // @RequestMapping("/") //
 	public String openForm() {
 		return "welcome";
@@ -35,7 +20,7 @@ public class StudentContoller {
 		return "studfrom";
 	}
 
-	// @RequestMapping(value = "/save", method = RequestMethod.POST)
+	// @RequestMapping(value = "/save", method =  RequestMethod.POST)
 	@PostMapping("/save")
 	public String saveRecord(@ModelAttribute("stud") Student student, Model model) {
 		System.out.println("  ==>  " + student);
